7f0087ab2af2f334bff6696fa29ce1786d0ab355
Mohamad Alkel <arcmohammadalkl@gmail.com>
2019-04-21 00:41:19 +0800

    feat/navbar

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import  Navbars  from '../containers/Navbars'
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div >
+        <Navbars />
+      </div>
+    )
+  }
+}
+
+
+export default Home;
