8362bc0ba0b6fb113f13626148075a2d2cb6129d
Mohamad Alkel <arcmohammadalkl@gmail.com>
2019-05-14 13:23:45 +0800

    edit style for profile page

--- my-app/src/pages/Portfolio.js
+++ my-app/src/pages/Portfolio.js
@@ -11,6 +11,7 @@ import laz from '../styles/img/laz.jpg'
 import GLLERY from '../styles/img/GLLERY.jpg'
 import YUMMY from '../styles/img/YUMMY.jpg'
 import pro from '../styles/img/pro.jpg'
+import foody from '../styles/img/foody.jpg'
 
 
 
@@ -21,169 +22,181 @@ class Profile extends Component {
     return (
     <div>
       
-      <CardColumns  className="topSpace ml-4 mr-4">
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={wedo} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | Wedo </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/72999029/Daily-Tasks" target="_blank"  className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={wush} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | Wushoo </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/72798271/Wushoo" target="_blank"  className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={Jubba} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> |  Jubba airways </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/72807405/Redesign-Jubba-airways" target="_blank" className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={IPLACE} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | IPLACE </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/71292473/IPLACE" target="_blank" className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={laz} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | LAZADA </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/71899325/Redesign-LAZADA" target="_blank" className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={GLLERY} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | MUEBLE GLLERY </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/70379393/MUEBLE-GLLERY" target="_blank" className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={YUMMY} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | YUMMY </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="https://www.behance.net/gallery/69392387/YUMMY" target="_blank"  className="btns m-3">See all Project</Button>
-      </div>
-      </Card>
-
-      <Card className=" color ">
-      <div className="row">
-        <div className="col-xl-8 pr-0">
-            <div className="imgCard ">
-                <CardImg className="border" top  src={pro} alt="Card image cap" />
-            </div>
-        </div>
-        <div className="col-xl-4 pl-0 pr-1">
-            <CardBody>
-            <CardTitle className="top"> | My Profile </CardTitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
-            <CardSubtitle className="textCard pl-3 mt-2"> Front-end dev</CardSubtitle>
-            
-            </CardBody>
-        </div>
-      </div>
-      <div className="row">
-         <Button href="/" target="_blank" className="btns m-3">See all Project</Button>
-      </div>
-      </Card>      
+      <CardColumns  className="topSpace ml-4 mr-4">    
       
+
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={foody} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | Foody </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> Front-end dev</CardSubtitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> Back-end dev</CardSubtitle>
+           
+              <Button href="https://foody-recipe.herokuapp.com/Favorite" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+  
+
+
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={pro} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | My Profile </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> Front-end dev</CardSubtitle>
+              <Button href="/" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={YUMMY} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | YUMMY </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+           
+              <Button href="https://www.behance.net/gallery/69392387/YUMMY" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={laz} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | LAZADA </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+            
+              <Button href="https://www.behance.net/gallery/71899325/Redesign-LAZADA" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={IPLACE} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | IPLACE </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+            
+              <Button href="https://www.behance.net/gallery/71292473/IPLACE" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={GLLERY} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | MUEBLE GLLERY </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+             
+              <Button href="https://www.behance.net/gallery/70379393/MUEBLE-GLLERY" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={wedo} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | Wedo </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+             
+              <Button href="https://www.behance.net/gallery/72999029/Daily-Tasks" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={Jubba} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> |  Jubba airways </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+ 
+              <Button href="https://www.behance.net/gallery/72807405/Redesign-Jubba-airways" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      <Card className=" flip ">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={wush} className="imgFlip"  alt="Avatar" />
+            </div>
+            <div class="flip-card-back">
+            <CardTitle className="top"> | Wushoo  </CardTitle>
+              <CardSubtitle className="textCard pl-3 mt-2"> UI UX Design</CardSubtitle>
+
+              <Button href="https://www.behance.net/gallery/72798271/Wushoo" target="_blank" className="btnFlip m-3">
+                See all Project
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+
+
+
     </CardColumns>
       
       </div>
